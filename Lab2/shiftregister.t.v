54863979d3d583eedb11b19d6a1e4218899ffae5
Griffin Tschurwald <griffin.tschurwald@gmail.com>
2015-11-02 00:54:09 -0500

    shift register is working with test benches

--- Lab2/shiftregister.t.v
+++ Lab2/shiftregister.t.v
@@ -4,14 +4,17 @@
 
 module testshiftregister();
 
-    reg             clk;
-    reg             peripheralClkEdge;
-    reg             parallelLoad;
+    wire             clk;
+    wire            peripheralClkEdge;
+    wire             parallelLoad;
     wire[7:0]       parallelDataOut;
     wire            serialDataOut;
-    reg[7:0]        parallelDataIn;
-    reg             serialDataIn; 
+    wire[7:0]        parallelDataIn;
+    wire             serialDataIn; 
     
+  reg		begintest;	// Set High to begin testing register file
+  wire		dutpassed;	// Indicates whether register file passed tests
+  wire 		endtest;
     // Instantiate with parameter width = 8
     shiftregister #(8) dut(.clk(clk), 
     		           .peripheralClkEdge(peripheralClkEdge),
@@ -19,11 +22,188 @@ module testshiftregister();
     		           .parallelDataIn(parallelDataIn), 
     		           .serialDataIn(serialDataIn), 
     		           .parallelDataOut(parallelDataOut), 
-    		           .serialDataOut(serialDataOut));
+    		           .serialDataOut(serialDataOut)
+	);
     
-    initial begin
-    	// Your Test Code
-    end
+shiftregisterTestBench tester
+  (
+    .begintest(begintest),
+    .endtest(endtest), 
+    .dutpassed(dutpassed),
+    .clk(clk), 
+    .peripheralClkEdge(peripheralClkEdge),
+    .parallelLoad(parallelLoad), 
+    .parallelDataIn(parallelDataIn), 
+    .serialDataIn(serialDataIn), 
+    .parallelDataOut(parallelDataOut), 
+    .serialDataOut(serialDataOut)
+);
+ 
+// Test harness asserts 'begintest' for 1000 time steps, starting at time 10
+  initial begin
+    begintest=0;
+    #10;
+    begintest=1;
+    #1000;
+  end
+
+  // Display test results ('dutpassed' signal) once 'endtest' goes high
+  always @(posedge endtest) begin
+    $display("DUT passed?: %b", dutpassed);
+  end
+    
 
 endmodule
 
+module shiftregisterTestBench(
+		// Test bench driver signal connections
+input	   		begintest,	// Triggers start of testing
+output reg 		endtest,	// Raise once test completes
+output reg 		dutpassed,	// Signal test result
+
+//shift register dut connections
+output reg		clk,
+output reg		peripheralClkEdge,
+output reg		parallelLoad, 
+output reg[7:0]		parallelDataIn,
+output reg		serialDataIn,
+input			serialDataOut,
+input[7:0]		parallelDataOut
+);
+
+integer i = 0;
+
+//set initial conditions of all register signals
+initial begin
+	clk = 0;
+	peripheralClkEdge = 0; //this triggers serial load of serialDataIn
+	parallelLoad = 0;
+	parallelDataIn = 8'h00;
+	serialDataIn = 0;
+
+end
+
+always @(posedge begintest) begin
+    endtest = 0;
+    dutpassed = 1;
+    #10
+
+//test case 1
+//serial in 8 0's and parallel read it
+#5
+serialDataIn = 0;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+serialDataIn = 1;
+peripheralClkEdge = 0; //check to make sure it won't load stuff in when this is 0
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+serialDataIn = 0;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+serialDataIn = 0;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+serialDataIn = 0;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+serialDataIn = 0;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+serialDataIn = 0;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+serialDataIn = 0;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+serialDataIn = 0;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+//now parallel read it
+if((parallelDataOut != 8'h00)) begin
+	dutpassed = 0;
+	$display("Test case 0 failed");
+end
+
+//test case 1
+//serial in 8 1's and parallel read it
+#5 clk = 0;
+$display("test case 1 is : %b", parallelDataOut);
+serialDataIn = 1;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+$display("test case 1 is : %b", parallelDataOut);
+serialDataIn = 0;
+peripheralClkEdge = 0; //check to make sure it won't load stuff in when this is 0
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+$display("test case 1 is : %b", parallelDataOut);
+serialDataIn = 1;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+$display("test case 1 is : %b", parallelDataOut);
+serialDataIn = 1;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+serialDataIn = 1;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+serialDataIn = 1;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+serialDataIn = 1;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+serialDataIn = 1;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+serialDataIn = 1;
+peripheralClkEdge = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+//now parallel read it
+if((parallelDataOut != 8'hff)) begin
+	dutpassed = 0;
+	$display("Test case 1` failed");
+	$display("paralledl data out is %h", parallelDataOut);
+end
+
+
+//test case 2
+//parallel in, serial out
+//read a parallel in, then read each out as we push garbage in
+#5
+parallelDataIn = 8'haa;
+parallelLoad = 1;
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+#5 clk = 1; #5 clk = 0; //generate clock pulse
+parallelLoad = 0;
+//now we need to read serial, then push garbage in
+//we'll need to check an if statement for each serial out
+
+ for (i = 0; i < 8; i = i + 1) begin
+	$display("modulo of i is %d", (i%2));
+	$display("parallelDataIn = %b", parallelDataIn);
+	$display("parallelDataOut = %b", parallelDataOut);
+	if(serialDataOut == (i%2)) begin
+	dutpassed = 0;
+		$display("Test case 2` failed");
+		$display("MSB that failed is : %b", serialDataOut);
+	end
+	//now load a garbage thing in 
+	$display("doing serialIn stuff");
+	serialDataIn = 0;
+	peripheralClkEdge = 1;
+	#5 clk = 1; #5 clk = 0; //generate clock pulse
+	peripheralClkEdge = 0;
+	#5 clk = 1; #5 clk = 0; //generate clock pulse
+end
+
+
+
+  // All done!  Wait a moment and signal test completion.
+  #5
+  endtest = 1;
+
+end
+
+endmodule
--- Lab2/shiftregister.v
+++ Lab2/shiftregister.v
@@ -14,8 +14,8 @@ input               peripheralClkEdge,  // Edge indicator - when this goes high,
 input               parallelLoad,       // 1 = Load shift reg with parallelDataIn
 input  [width-1:0]  parallelDataIn,     // Load shift reg in parallel
 input               serialDataIn,       // Load shift reg serially
-output [width-1:0]  parallelDataOut,    // Shift reg data contents
-output              serialDataOut       // Positive edge synchronized
+output reg[width-1:0]  parallelDataOut,    // Shift reg data contents
+output reg             serialDataOut       // Positive edge synchronized
 );
 
     reg [width-1:0]      shiftregistermem;
@@ -23,18 +23,18 @@ output              serialDataOut       // Positive edge synchronized
         // Your Code Here
 	// this is basically 8 flip flops in a row
 
-	if (peripheralClkEdge == 1) //when the peripheralClkEdge goes high
+	if (peripheralClkEdge == 1) begin //when the peripheralClkEdge goes high
 	//serial load to LSB  here
-	shiftregistermem <= {shiftregistermem[width-2:0], serialDataIn);
+	shiftregistermem <= {shiftregistermem[width-2:0], serialDataIn};
 	end
 
-	if (parallelLoad == 1)
+	if (parallelLoad == 1) begin
 	//load it in parallel
 	shiftregistermem <= parallelDataIn;
 	end
 
 	//assign the right numbers to the outputs every clock cycle
-	parallelDataOut <= shiftregistermem;
+	parallelDataOut <= shiftregistermem[7:0];
 	serialDataOut <= shiftregistermem[width-1];
     end
 endmodule
